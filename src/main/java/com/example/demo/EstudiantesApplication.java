60ef7a9cf25f17922a51ba78a4a8c91dc5f751b5
Célita Morales <cmoralesm37@miumg.edu.gt>
2022-06-11 01:16:07 -0600

    correcciones

--- src/main/java/com/example/demo/EstudiantesApplication.java
+++ src/main/java/com/example/demo/EstudiantesApplication.java
@@ -2,6 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -25,7 +33,7 @@ public class EstudiantesApplication {
 	private ApiInfo apiDetails() {
 		return new ApiInfoBuilder()
 				.title("api estudiantes")
-				.contact(new Contact("Charles","no-url", "myemail"))
+				.contact(new Contact("Célita","no-url", "myemail"))
 				.description("Muestra api SPRINGBOOT")
 				.build();
 	}
